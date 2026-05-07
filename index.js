4028d15426d6bbf21e8dafb97f63ccc76ee3a69c
Adinarayan <adinarayanp2020@gmail.com>
2026-05-07 14:28:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,158 @@
+/*console.log('Hello JavaScript');
+console.log(2+5);
+let c=2;
+if (c % 2 == 0) {
+    console.log('Number is even');
+} else {
+    console.log('Number is odd');
+}
+let d=2;
+let e=3;
+if (d>e) {
+    console.log('2 id larger');
+} else {
+    console.log('3 is larger');
+}
+let a='name';
+let b='age';
+let f='place';
+console.log(a,b,c);
+let n=2;
+let m=3;
+console.log('original order')
+console.log(m,n);
+let a=n;
+let b=m;
+m=a;
+n=b;
+console.log('swapped')
+console.log(m,n);
+
+let g=2;
+if(g<0)
+    console.log('G is negative')
+else if(g>0)
+    console.log('G is positive')
+else
+    console.log('G is zero')
+let h=0;
+for(let i=0;i<=10;i++)
+    h=h+i
+console.log(h);
+for(let i=0;i<=10;i++)
+    console.log(i);
+for(let i=0;i<=50;i++)
+    if(i%2==0)
+        console.log(i);
+        
+console.log('Black')
+
+let a=2;
+let b=5;
+if(a>b)
+    console.log('A is larger')
+else if(a<b)
+    console.log('A is smaller')
+else
+    console.log('A equals B')
+    
+let c=69;
+if(c>=18)
+    console.log('Eligible to vote')
+else
+    console.log('Ineligible to vote')
+let d=1;
+let e=2;
+let f=3;
+let Average=(d+e+f)/3
+console.log(Average)
+
+let f=0;
+let c=8;
+f=c*(9/5)+32
+console.log('Farenheit=',f)
+
+let m=678;
+let s=m*60;
+console.log('converted value:',s)
+
+let l=5;
+let b=6;
+console.log('Area of a rectangle is:',l*b)
+*/
+/*function Adi(){
+    console.log('Yo soy Adinarayan');
+}
+Adi();*/
+/*
+function hw(){
+    console.log('Hello World');
+}
+hw();*/
+/*function Adi(a,b){
+    let c=a+b;
+    console.log(c);
+}
+Adi(8,9);*/
+/*function Adi(a,b){
+    let c=a-b;
+    console.log(c);
+}
+Adi(8,9);*/
+/*function Adi(a,b){
+    let c=a*b;
+    console.log(c);
+}
+Adi(8,9);*/
+/*function Adi(a,b){
+    let c=a/b;
+    console.log(c);
+}
+Adi(8,9);*/
+/*function Adi(a){
+    let c=a*a;
+    console.log(c);
+}
+Adi(8);*/
+/*function Adi(a){
+    let c=a*a*a;
+    console.log(c);
+}
+Adi(8);*/
+/*function Adi(a){
+    if(a%2==0)
+        console.log('even');
+    else
+        console.log('odd')
+}
+Adi(8);*/
+/*function Adi(a,b){
+    if(a>b)
+        console.log(a ,'larger');
+    else
+        console.log(b, 'larger')
+}
+Adi(8,4);*/
+/*function Adi(a,b){
+    if(a<b)
+        console.log(a ,'smaller');
+    else
+        console.log(b, 'smaller')
+}
+Adi(8,4);*/
+/*function Adi(a){
+    if(a<0)
+        console.log(a ,'is -ve');
+    else if(a>0)
+        console.log(a, 'is +ve')
+    else
+        console.log(a, 'is 0')
+}
+Adi(8);*/
+/*function Adi(){
+    for(let i=1;i<=10;i++)
+        console.log(i)
+}
+Adi();*/
+
+
